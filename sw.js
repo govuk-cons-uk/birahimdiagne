fb6e8bdf860a0a6d30de9fa50d5bbd0bc5fd10cf
govuk-cons-uk <frontdeveloppeur7@gmail.com>
2026-05-07 21:41:48 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
-self.addEventListener('fetch', function(event) {
-  // Ce script permet au site de fonctionner hors ligne si nécessaire
+const CACHE_NAME = 'birahim-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Installation du Service Worker et mise en cache
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Récupération des ressources
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
